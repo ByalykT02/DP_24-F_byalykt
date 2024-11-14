45206349e48ed8ff3a37d8762f3771a4a0a901e0
ByalykT01 <byalykt@hotmail.com>
2024-11-15 00:18:55 +0100

    some check for the error

--- src/server/db/queries/greeting.ts
+++ src/server/db/queries/greeting.ts
@@ -4,10 +4,10 @@ import { db } from "..";
 export async function getGreeting(): Promise<string>{
   try {
     const result = await db.query.greeting.findFirst();
-    if (!result?.greetingText) return "Hello, World(not from db)";
+    if (!result?.greetingText) return "Hello, World(not from db 1)";
 
     return result.greetingText;
   } catch (e) {
-    return "Hello, World(not from db)";
+    return "Hello, World(not from db 2)";
   }
 }
