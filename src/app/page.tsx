e2c09b8156f3624438de5a69cea08396d5fd0ff8
ByalykT01 <byalykt@hotmail.com>
2025-02-14 09:23:07 +0100

    feat: included SWR in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,39 @@
 "use client";
-
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React from "react";
 import { Artwork } from "~/lib/types/artwork";
 import { Loading } from "~/components/ui/loading";
 import Hero from "~/components/home/hero";
 import ArtworkTiles from "~/components/home/arwork-tiles";
 import { fetchArtworks } from "~/server/actions/fetch-artworks-home";
+import useSWR from "swr";
 
 const MainPage = () => {
-  const [results, setResults] = useState<Artwork[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const loadingRef = useRef(false);
-
-  const loadArtworks = useCallback(async () => {
-    if (loadingRef.current) return;
-
-    try {
-      loadingRef.current = true;
-      const artworks = await fetchArtworks();
-      setResults(artworks);
-    } catch (error) {
-      console.error("Error loading artworks:");
-    } finally {
-      setIsLoading(false);
-      loadingRef.current = false;
+  const { 
+    data: artworks, 
+    isLoading, 
+    error 
+  } = useSWR<Artwork[]>('artworks', fetchArtworks, {
+    revalidateOnFocus: false,
+    dedupingInterval: 60 * 1000,
+    onError: (error) => {
+      console.error("Error loading artworks:", error);
     }
-  }, []);
-
-  useEffect(() => {
-    void loadArtworks();
-  }, [loadArtworks]);
+  });
 
   return (
     <>
       {isLoading && <Loading />}
       <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
-        <Hero artworks={results} />
-        <ArtworkTiles artworks={results} />
+        {error ? (
+          <div className="text-center text-red-600 p-4">
+            Failed to load artworks. Please try again later.
+          </div>
+        ) : (
+          <>
+            <Hero artworks={artworks ?? []} />
+            <ArtworkTiles artworks={artworks ?? []} />
+          </>
+        )}
       </div>
     </>
   );
